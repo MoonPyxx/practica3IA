d70dfcc25b7ffeecd339ca010232c598484ca212
moonpyx <moonpyx6@gmail.com>
2024-06-08 18:29:38 +0200

    eliminada heurística 2

--- include/AIPlayer.h
+++ include/AIPlayer.h
@@ -63,8 +63,6 @@ class AIPlayer: public Player{
         double Poda_AlfaBeta(const Parchis &estado, int jugador, int profundidad, int profundidad_max, color &mejor_color, int &mejor_id, int &mejor_dado, double alpha, double beta, double (*heuristica)(const Parchis &, int))const;
         // heuristica(s)
         static double MiValoracion1(const Parchis &estado, int jugador);
-        static double MiValoracion2(const Parchis &estado, int jugador);
-
 
         /**
          * @brief Método que determina si el player es inteligente (decide el mejor movimiento)
